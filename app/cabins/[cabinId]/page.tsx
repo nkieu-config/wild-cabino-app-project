21acb59653cf61c9d09d45518171591ec2540b45
nkieu-config <natthachak.config@gmail.com>
2025-08-20 03:43:02 +0700

    feat: add sidebar active highlight and TextExpander component

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { getCabin } from "@/app/_lib/data-service";
 import { getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import TextExpander from "@/app/_components/TextExpander";
 
 type PageProps = {
   params: Promise<{ cabinId: string }>;
@@ -44,7 +45,9 @@ export default async function Page({ params }: PageProps) {
             Cabin {name}
           </h3>
 
-          <p className="text-primary-300 mb-10 text-lg">{description}</p>
+          <p className="text-primary-300 mb-10 text-lg">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="mb-7 flex flex-col gap-4">
             <li className="flex items-center gap-3">
