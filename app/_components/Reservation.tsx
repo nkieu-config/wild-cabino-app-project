ca18c42d6d86173ed0ad8604a5997c7663fe2949
nkieu-config <natthachak.config@gmail.com>
2025-08-21 00:12:29 +0700

    feat: implement data fetching and add reservation flow with Cabins, DateSelector, and ReservationForm

---
+++ app/_components/Reservation.tsx
@@ -0,0 +1,23 @@
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import { Cabin } from "@/app/_lib/types";
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+
+async function Reservation({ cabin }: { cabin: Cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+  return (
+    <div className="border-primary-800 grid min-h-[400px] grid-cols-2 border">
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
+
+export default Reservation;
--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import { Suspense } from "react";
 import { getCabin } from "@/app/_lib/data-service";
 import { getCabins } from "@/app/_lib/data-service";
-import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
-import TextExpander from "@/app/_components/TextExpander";
+import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
+import Cabins from "@/app/_components/Cabins";
 
 type PageProps = {
-  params: Promise<{ cabinId: string }>;
+  params: Promise<{ cabinId: number }>;
 };
 
 export async function generateMetadata({ params }: PageProps) {
@@ -21,63 +22,20 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: PageProps) {
   const { cabinId } = await params;
-  const cabin = await getCabin(Number(cabinId));
-
-  const { id, name, maxCapacity, regularPrice, discount, image, description } =
-    cabin;
+  const cabin = await getCabin(cabinId);
 
   return (
     <div className="mx-auto mt-8 max-w-6xl">
-      <div className="border-primary-800 mb-24 grid grid-cols-[3fr_4fr] gap-20 border px-10 py-3">
-        <div className="relative -translate-x-3 scale-[1.15]">
-          <Image
-            className="object-cover"
-            src={image}
-            fill
-            priority
-            sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
-            alt={`Cabin ${name}`}
-          />
-        </div>
-
-        <div>
-          <h3 className="text-accent-100 bg-primary-950 mb-5 w-[150%] translate-x-[-254px] p-6 pb-1 text-7xl font-black">
-            Cabin {name}
-          </h3>
-
-          <p className="text-primary-300 mb-10 text-lg">
-            <TextExpander>{description}</TextExpander>
-          </p>
-
-          <ul className="mb-7 flex flex-col gap-4">
-            <li className="flex items-center gap-3">
-              <UsersIcon className="text-primary-600 h-5 w-5" />
-              <span className="text-lg">
-                For up to <span className="font-bold">{maxCapacity}</span>{" "}
-                guests
-              </span>
-            </li>
-            <li className="flex items-center gap-3">
-              <MapPinIcon className="text-primary-600 h-5 w-5" />
-              <span className="text-lg">
-                Located in the heart of the{" "}
-                <span className="font-bold">Dolomites</span> (Italy)
-              </span>
-            </li>
-            <li className="flex items-center gap-3">
-              <EyeSlashIcon className="text-primary-600 h-5 w-5" />
-              <span className="text-lg">
-                Privacy <span className="font-bold">100%</span> guaranteed
-              </span>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Cabins cabin={cabin} />
 
       <div>
-        <h2 className="text-center text-5xl font-semibold">
-          Reserve today. Pay on arrival.
+        <h2 className="text-accent-400 mb-10 text-center text-5xl font-semibold">
+          Reserve {cabin.name} today. Pay on arrival.
         </h2>
+
+        <Suspense fallback={<Spinner />}>
+          <Reservation cabin={cabin} />
+        </Suspense>
       </div>
     </div>
   );
