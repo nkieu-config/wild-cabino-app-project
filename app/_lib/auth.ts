d56c7ef441e69d1b58ebc134c75ee9f6a9b5a0d6
nkieu-config <natthachak.config@gmail.com>
2025-08-22 03:30:38 +0700

    feat: create new guest on first sign in and attach guestId to session

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
-import { NextAuthConfig } from "next-auth";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig: NextAuthConfig = {
   providers: [
@@ -12,6 +12,36 @@ const authConfig: NextAuthConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        if (!user.email) return false;
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({
+            email: user.email,
+            fullName: user.name ?? null,
+            nationalID: null,
+            nationality: null,
+            countryFlag: null,
+          });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      if (!session.user.email) return session;
+
+      const guest = await getGuest(session.user.email);
+
+      if (guest) {
+        session.user.guestId = guest.id;
+      }
+
+      return session;
     },
   },
   pages: {
